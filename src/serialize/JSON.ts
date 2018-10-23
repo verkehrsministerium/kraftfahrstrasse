0b69324e231f0222cd5c2ce952c29e3968e3333c
Martin Koppehel <martin.koppehel@st.ovgu.de>
2018-10-23 19:26:16 +0200

    Add JSON and msgpack serializer

---
+++ src/serialize/JSON.ts
@@ -0,0 +1,20 @@
+import { ITextSerializer } from '../types/Serializer';
+import { WampMessage } from '../types/Protocol';
+
+export class JSONSerializer implements ITextSerializer {
+  public IsBinary(): boolean {
+    return false;
+  }
+
+  public ProtocolID(): string {
+    return "wamp.2.json";
+  }
+
+  public Serialize(msg: WampMessage): string {
+    return JSON.stringify(msg);
+  }
+
+  public Deserialize(msg: string): WampMessage {
+    return JSON.parse(msg);
+  }
+}
---
+++ src/serialize/MSGPack.ts
@@ -0,0 +1,24 @@
+import * as msgpackFactory from 'msgpack5';
+import { IBinarySerializer } from '../types/Serializer';
+import { WampMessage } from '../types/Protocol';
+
+export class MSGPackSerializer implements IBinarySerializer {
+  private msgpack: msgpackFactory.MessagePack;
+  constructor() {
+    this.msgpack = msgpackFactory({
+      forceFloat64: true,
+    });
+  }
+  public IsBinary(): boolean {
+    return true;
+  }
+  public ProtocolID(): string {
+    return "wamp.2.msgpack";
+  }
+  public Serialize(msg: WampMessage): ArrayBufferLike {
+    return this.msgpack.encode(msg) as any;
+  }
+  public Deserialize(msg: ArrayBufferLike): WampMessage {
+    return this.msgpack.decode(msg as any);
+  }
+}
