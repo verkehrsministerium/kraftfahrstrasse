94aa51a698305f330d3932514bf03d4b5659fc9a
Martin Koppehel <martin.koppehel@st.ovgu.de>
2018-10-26 21:46:53 +0200

    Add public API

---
+++ example/index.ts
@@ -0,0 +1,53 @@
+import { NodeWebSocketTransport } from './transport/NodeWebSocketTransport';
+import { Connection } from './generic/Connection';
+import { ConnectionCloseInfo } from './types/Connection';
+import { JSONSerializer } from './serialize/JSON';
+import { AnonymousAuthProvider } from './auth/Anonymous';
+import { Deferred } from 'queueable';
+
+const connection = new Connection({
+  endpoint: "ws://localhost:4000",
+  serializer: new JSONSerializer(),
+  transport: NodeWebSocketTransport,
+  transportOptions: {},
+  authProvider: new AnonymousAuthProvider(),
+  logFunction: console.log as any,
+  realm: "robulab",
+});
+
+const main = async () => {
+  await connection.Open();
+  const sub = await connection.Subscribe("com.robulab.foo.baz", (args, kwargs, details) => {
+    console.log("Subscription:", args, kwargs, details);
+  }, {});
+  const reg = await connection.Register("com.robulab.foo.bar", async (args, kwargs, details) => {
+    console.log("Called with args:", args, kwargs, details);
+    await connection.Publish("com.robulab.foo.baz", args, kwargs, {
+      acknowledge: true,
+      disclose_me: true,
+    });
+    return {
+      args: null,
+      kwArgs: null,
+      nextResult: null,
+    };
+  }, {
+    disclose_caller: true,
+  });
+  setTimeout(async () => await connection.Call("com.robulab.foo.bar"), 1000);
+  setTimeout(() => sub.Unsubscribe().then(() => console.log("Unsubscribed")), 10000);
+  const [res, cid] = connection.Call("com.robulab.target.get-online");
+  console.log(await res, cid);
+  const closer = new Deferred<ConnectionCloseInfo>();
+  setTimeout(async () => {
+    await reg.Unregister();
+    connection.Close().then(closer.resolve, closer.reject);
+  }, 20000);
+  console.log(await closer);
+}
+
+main().then(() => {});
+
+
+
+//setTimeout(() => connection.Close().then(console.log.bind(null, "Connection closed")), 10000);
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,36 @@
-import { NodeWebSocketTransport } from './transport/NodeWebSocketTransport';
-import { Connection } from './generic/Connection';
-import { ConnectionCloseInfo } from './types/Connection';
-import { JSONSerializer } from './serialize/JSON';
-import { AnonymousAuthProvider } from './auth/Anonymous';
-import { Deferred } from 'queueable';
+/*
+ * kraftfahrstrasse - Public API
+ * (c) 2018
+ * Fin Christensen
+ * Martin Koppehel
+ * Johann Wagner
+ * Marten Wallewein-Eising
+ * Johannes Wünsche
+ */
 
-const connection = new Connection({
-  endpoint: "ws://localhost:4000",
-  serializer: new JSONSerializer(),
-  transport: NodeWebSocketTransport,
-  transportOptions: {},
-  authProvider: new AnonymousAuthProvider(),
-  logFunction: console.log as any,
-  realm: "robulab",
-});
+// Export basic types
+export { WampID, WampURI, WampDict, WampList } from './types/messages/MessageTypes';
 
-const main = async () => {
-  await connection.Open();
-  const sub = await connection.Subscribe("com.robulab.foo.baz", (args, kwargs, details) => {
-    console.log("Subscription:", args, kwargs, details);
-  }, {});
-  const reg = await connection.Register("com.robulab.foo.bar", async (args, kwargs, details) => {
-    console.log("Called with args:", args, kwargs, details);
-    await connection.Publish("com.robulab.foo.baz", args, kwargs, {
-      acknowledge: true,
-      disclose_me: true,
-    });
-    return {
-      args: null,
-      kwArgs: null,
-      nextResult: null,
-    };
-  }, {
-    disclose_caller: true,
-  });
-  setTimeout(async () => await connection.Call("com.robulab.foo.bar"), 1000);
-  setTimeout(() => sub.Unsubscribe().then(() => console.log("Unsubscribed")), 10000);
-  const [res, cid] = connection.Call("com.robulab.target.get-online");
-  console.log(await res, cid);
-  const closer = new Deferred<ConnectionCloseInfo>();
-  setTimeout(async () => {
-    await reg.Unregister();
-    connection.Close().then(closer.resolve, closer.reject);
-  }, 20000);
-  console.log(await closer);
-}
+// Export public interfaces to interact with the library
+export * from './types/Serializer';
+export * from './types/Transport';
+export * from './types/AuthProvider';
+export * from './types/Connection';
 
-main().then(() => {});
+// Export different transports
+export { BrowserWebSocketTransport } from './transport/BrowserWebSocketTransport';
+export { NodeWebSocketTransport } from './transport/NodeWebSocketTransport';
 
+// Export different serializers
+export { JSONSerializer } from './serialize/JSON';
+export { MSGPackSerializer } from './serialize/MSGPack';
 
+// Export the different authentication providers
+export { TransportLevelProvider } from './auth/TransportProvider';
+export { TLSAuthProvider } from './auth/TLS';
+export { TicketAuthProvider } from './auth/Ticket';
+export { CookieAuthProvider } from './auth/Cookie';
+export { AnonymousAuthProvider } from './auth/Anonymous';
 
-//setTimeout(() => connection.Close().then(console.log.bind(null, "Connection closed")), 10000);
+// Export the main connection object
+export { Connection } from './generic/Connection';
