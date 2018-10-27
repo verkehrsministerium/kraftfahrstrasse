d8ac15199c9de37ca4d875ec8966518101f6c93b
Martin Koppehel <martin.koppehel@st.ovgu.de>
2018-10-27 17:01:02 +0200

    Refactor node and browser implementation into a generic one

--- src/transport/NodeWebSocketTransport.ts
+++ src/transport/NodeWebSocketTransport.ts
@@ -1,83 +1,11 @@
-import { Channel } from 'queueable';
 import * as WebSocket from 'ws';
 
-import { WampMessage } from '../types/Protocol';
-import { IsBinarySerializer, ISerializer } from '../types/Serializer';
-import { ETransportEventType, ITransport, TransportEvent } from '../types/Transport';
-
+import { ISerializer } from '../types/Serializer';
+import { WebSocketTransport } from './WebSocketTransport';
 import { WampDict } from '../types/messages/MessageTypes';
 
-// TODO: Add ping-pong.
-
-export class NodeWebSocketTransport implements ITransport {
-  private webSocket: WebSocket | null;
-  private channel = new Channel<TransportEvent>();
-  constructor(private serializer: ISerializer, _: WampDict) {
-  }
-
-  public Open(endpoint: string): AsyncIterableIterator<TransportEvent> {
-    if (!!this.webSocket) {
-      const channel = new Channel<TransportEvent>();
-      channel.push({
-        type: ETransportEventType.ERROR,
-
-        error: 'Transport already opened!',
-      });
-      return channel;
-    }
-
-    this.webSocket = new WebSocket(endpoint, this.serializer.ProtocolID());
-    if (IsBinarySerializer(this.serializer)) {
-      this.webSocket.binaryType = 'arraybuffer';
-    }
-    this.webSocket.onopen = () => {
-      this.channel.push({
-        type: ETransportEventType.OPEN,
-      });
-    };
-    this.webSocket.onmessage = ev => {
-      try {
-        this.channel.push({
-          type: ETransportEventType.MESSAGE,
-
-          // FIXME: Report to TSC
-          message: (this.serializer.Deserialize as any)(ev.data),
-        });
-      } catch (err) {
-        this.channel.push({
-          type: ETransportEventType.ERROR,
-
-          error: err,
-        });
-      }
-    };
-    this.webSocket.onclose = ev => {
-      this.webSocket.onclose = null;
-      this.channel.push({
-        type: ETransportEventType.CLOSE,
-
-        code: ev.code,
-        reason: ev.reason,
-        wasClean: ev.wasClean,
-      });
-    };
-    return this.channel;
-  }
-
-  public Close(code: number, reason: string): void {
-    this.webSocket.onclose = null;
-    this.webSocket.close(code, reason);
-    this.channel.push({
-      type: ETransportEventType.CLOSE,
-
-      code,
-      reason,
-      wasClean: true,
-    });
-  }
-
-  public Send(msg: WampMessage): void {
-    const payload = this.serializer.Serialize(msg);
-    this.webSocket.send(payload);
+export class NodeWebSocketTransport extends WebSocketTransport {
+  constructor(serializer: ISerializer, _: WampDict) {
+    super(serializer, WebSocket as any);
   }
 }
---
+++ src/transport/WebSocketTransport.ts
@@ -0,0 +1,82 @@
+import { Channel } from 'queueable';
+
+import { WampMessage } from '../types/Protocol';
+import { IsBinarySerializer, ISerializer } from '../types/Serializer';
+import { ETransportEventType, ITransport, TransportEvent } from '../types/Transport';
+
+export interface IWebSocketFactory {
+  new (endpoint: string, protocol?: string | string[]): WebSocket;
+}
+
+export abstract class WebSocketTransport implements ITransport {
+  protected webSocket: WebSocket;
+  private channel = new Channel<TransportEvent>();
+  constructor(private serializer: ISerializer, private webSocketFactory: IWebSocketFactory) {
+  }
+
+  public Open(endpoint: string): AsyncIterableIterator<TransportEvent> {
+    if (!!this.webSocket) {
+      const channel = new Channel<TransportEvent>();
+      channel.push({
+        type: ETransportEventType.ERROR,
+
+        error: 'Transport already opened!',
+      });
+      return channel;
+    }
+
+    this.webSocket = new this.webSocketFactory(endpoint, this.serializer.ProtocolID());
+    if (IsBinarySerializer(this.serializer)) {
+      this.webSocket.binaryType = 'arraybuffer';
+    }
+    this.webSocket.onopen = () => {
+      this.channel.push({
+        type: ETransportEventType.OPEN,
+      });
+    };
+    this.webSocket.onmessage = ev => {
+      try {
+        this.channel.push({
+          type: ETransportEventType.MESSAGE,
+
+          // FIXME: Report to TSC
+          message: (this.serializer.Deserialize as any)(ev.data),
+        });
+      } catch (err) {
+        this.channel.push({
+          type: ETransportEventType.ERROR,
+
+          error: err,
+        });
+      }
+    };
+    this.webSocket.onclose = ev => {
+      this.webSocket.onclose = null;
+      this.channel.push({
+        type: ETransportEventType.CLOSE,
+
+        code: ev.code,
+        reason: ev.reason,
+        wasClean: ev.wasClean,
+      });
+    };
+    return this.channel;
+  }
+
+  public Close(code: number, reason: string): void {
+    this.webSocket.onclose = null;
+    this.webSocket.close(code, reason);
+    this.channel.push({
+      type: ETransportEventType.CLOSE,
+
+      code,
+      reason,
+      wasClean: true,
+    });
+  }
+
+  public Send(msg: WampMessage): void {
+    const payload = this.serializer.Serialize(msg);
+    this.webSocket.send(payload);
+  }
+}
