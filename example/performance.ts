70f56048a03fb832ad6dfad6aae2898669202538
Fin Christensen <christensen.fin@gmail.com>
2019-07-28 14:41:04 +0200

    Better output in performance example

--- example/performance.ts
+++ example/performance.ts
@@ -7,6 +7,10 @@ import {
 
 const ROUTER_ADDRESS = process.env.ROUTER_ADDRESS || process.exit(1);
 
+function getTimestamp() {
+  return +new Date();
+}
+
 async function main() {
   const connection = new Connection({
     endpoint: ROUTER_ADDRESS,
@@ -14,6 +18,9 @@ async function main() {
 
     serializer: new JSONSerializer(),
     transport: NodeWebSocketTransport,
+    transportOptions: {
+      perMessageDeflate: false,
+    },
     authProvider: new AnonymousAuthProvider(),
 
     logFunction: (level: String, timestamp: Date, file: String, msg: String) => {
@@ -38,14 +45,16 @@ async function main() {
   );
 
   let msgs = 0;
-
-  setInterval(() => {
-    console.log(`${msgs} per second`);
-    msgs = 0;
-  }, 1000);
-
+  let time = getTimestamp();
   console.log("publishing");
+
   while (true) {
+    let now = getTimestamp();
+    if (now - time > 1000) {
+      console.log(`${msgs} in ${(now - time) / 1000} second(s)`);
+      msgs = 0;
+      time = now;
+    }
     try {
       await connection.Publish('scenario.high_load');
       msgs += 1;
